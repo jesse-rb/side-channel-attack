0a0c6f108b24d056097b589424e97342d1f3ce28
jesse-rb <jesse.reynekebarnard@gmail.com>
2021-10-07 18:07:33 +1300

    needs work

--- main.go
+++ main.go
@@ -3,39 +3,32 @@ package main
 import (
 	"fmt"
 	"time"
+	"log"
 )
 
 func experiment(bytes []byte, steps int, incr int) (time.Duration) {
-	begin := time.Now()
-
-	var index int = 0
-	for i := 0; i < len(bytes)-1; i++ {
-		if index < len(bytes)-1 { _ = bytes[index] }
-		index += incr
-		if index >= len(bytes) { index = 0 }
-	}
-
-	elapsed := time.Since(begin)
-	return elapsed
-}
-
-func average(bytes []byte, steps int, incr int) (time.Duration) {
 	var nanoSecondsAvg int64 = 0;
-	var iterations int64 = 50
-	var i int64 = 0
-	for i = 0; i < iterations; i++ {
-		nanoSecondsAvg += experiment(bytes, steps, incr).Nanoseconds()
+	var lengthMod int = len(bytes)-1
+	for i := 0; i < len(bytes)-1; i++ {
+		begin := time.Now()
+		bytes[(i*incr)&lengthMod]++
+		elapsed := time.Since(begin)
+		nanoSecondsAvg += elapsed.Nanoseconds()
 	}
-	return (time.Duration(nanoSecondsAvg/iterations))
+
+	var total int = len(bytes)
+	nanoSecondsAvg = nanoSecondsAvg/(int64(total))
+	return time.Duration(nanoSecondsAvg)
 }
 
 func main() {
-	fmt.Printf("Time (Microseconds),Array Size (Bytes),Steps,Increment\n")
-	for i := 0; i < 100; i++ {
-		var bytes		[]byte	= make([]byte, i*1024)
-		var steps	int 	= i*1024
-		var incr	int		= i*16	
-		elapsed := average(bytes, steps, incr)
-		fmt.Printf("%d,%d,%d,%d\n", elapsed.Microseconds(), len(bytes), steps, incr)
+	fmt.Printf("Time (Nanoseconds),Array Size (Bytes),St eps,Increment\n")
+	for i := 1; i <= 100; i++ {
+		var bytes	[]byte	= make([]byte, i*1024*1024)
+		var steps	int 	= i*1024*1024
+		var incr	int		= 16
+		elapsed := experiment(bytes, steps, incr)
+		log.Println(fmt.Sprintf("%d", i));
+		fmt.Printf("%d,%d,%d,%d\n", elapsed.Nanoseconds(), len(bytes), steps, incr)
 	}
 }
---
+++ report.md
@@ -0,0 +1,17 @@
+# Report
+
+```
+L1d cache:                       192 KiB
+L1i cache:                       192 KiB
+L2 cache:                        1.5 MiB
+L3 cache:                        12 MiB
+```
+
+## Calculate cache sizes in bytes
+L1d = 196608 B
+L1i = 196608 B
+L1d + L1i = 393216 B
+L2 = 1.573e+6 B = 1573000 B
+L3 = 1.258e+7 B = 12580000 B
+
+Total = 393216 B + 1573000 B + 12580000 B = 14546216 B
