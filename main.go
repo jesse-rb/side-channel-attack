94c0924fd43ca989ab443da5f6d3af14d63f83f7
jesse-rb <jesse.reynekebarnard@gmail.com>
2021-10-08 21:50:08 +1300

    progress

--- main.go
+++ main.go
@@ -7,28 +7,30 @@ import (
 )
 
 func experiment(bytes []byte, steps int, incr int) (time.Duration) {
-	var nanoSecondsAvg int64 = 0;
 	var lengthMod int = len(bytes)-1
-	for i := 0; i < len(bytes)-1; i++ {
-		begin := time.Now()
-		bytes[(i*incr)&lengthMod]++
-		elapsed := time.Since(begin)
-		nanoSecondsAvg += elapsed.Nanoseconds()
-	}
+	var elapsedAvg int64 = 0
 
-	var total int = len(bytes)
-	nanoSecondsAvg = nanoSecondsAvg/(int64(total))
-	return time.Duration(nanoSecondsAvg)
+	for i := 0; i <= 50; i++ {
+		begin := time.Now()
+		for i := 0; i < len(bytes)-1; i++ {
+			bytes[(i*incr)&lengthMod]++ // Fast modulus operation
+			// log.Println(fmt.Sprintf("%d", bytes[(i*incr)&lengthMod]))
+		}
+		elapsedAvg += time.Since(begin).Nanoseconds()
+	}
+	
+	elapsedAvg = elapsedAvg/int64(len(bytes))
+	return time.Duration(elapsedAvg)
 }
 
 func main() {
-	fmt.Printf("Time (Nanoseconds),Array Size (Bytes),St eps,Increment\n")
-	for i := 1; i <= 100; i++ {
+	fmt.Printf("Time (Nanoseconds),Array Size (MegaBytes),St eps,Increment\n")
+	for i := 1; i <= 32; i++ {
 		var bytes	[]byte	= make([]byte, i*1024*1024)
-		var steps	int 	= i*1024*1024
+		var steps	int 	= 64 * 1024 * 1024
 		var incr	int		= 16
 		elapsed := experiment(bytes, steps, incr)
 		log.Println(fmt.Sprintf("%d", i));
-		fmt.Printf("%d,%d,%d,%d\n", elapsed.Nanoseconds(), len(bytes), steps, incr)
+		fmt.Printf("%d,%d,%d,%d\n", elapsed.Nanoseconds(), len(bytes)/(1024*1024), steps, incr)
 	}
 }
--- report.md
+++ report.md
@@ -1,5 +1,6 @@
 # Report
 
+# lscpu
 ```
 L1d cache:                       192 KiB
 L1i cache:                       192 KiB
@@ -8,6 +9,8 @@ L3 cache:                        12 MiB
 ```
 
 ## Calculate cache sizes in bytes
+
+```
 L1d = 196608 B
 L1i = 196608 B
 L1d + L1i = 393216 B
@@ -15,3 +18,4 @@ L2 = 1.573e+6 B = 1573000 B
 L3 = 1.258e+7 B = 12580000 B
 
 Total = 393216 B + 1573000 B + 12580000 B = 14546216 B
+```
